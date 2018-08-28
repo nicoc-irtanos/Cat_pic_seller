5a6af9e4dc2a5bd61cf30485ac6d4ebfd08ac50a
bbpucca <puccafit@gmail.com>
2018-08-28 15:39:38 +0200

    cart template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
   def index
   end
+  def basket
+  end
+  def checkout
+  end
+  def profil
+  end
+
   def product_europeen
   end
   def product_siberien
   end
   def product_persan
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get '/product_europeen', to: 'home#product_europeen'
   get '/product_siberien', to: 'home#product_siberien'
   get '/product_persan', to: 'home#product_persan'
+  get '/basket', to: 'home#basket'
+  get '/checkout', to: 'home#checkout'
+  get '/profil', to: 'home#profil'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
